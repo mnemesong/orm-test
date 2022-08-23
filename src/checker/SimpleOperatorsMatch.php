ac6b0d0e55d54a6a60b42d865128f77cdc46c3fd
Pantagruel74 <tostar74@mail.ru>
2022-08-24 03:54:17 +0500

    Essence logic of comparing values with simple operators

---
+++ src/checker/SimpleOperatorsMatch.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Mnemesong\OrmTest\checker;
+
+use Webmozart\Assert\Assert;
+
+class SimpleOperatorsMatch
+{
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) && is_null($val2)) {
+            return true;
+        }
+        if(is_null($val1) || is_null($val2)) {
+            return false;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 == $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) === 0;
+        }
+        return $val1 === $val2;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forNotEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) && is_null($val2)) {
+            return false;
+        }
+        if(is_null($val1) || is_null($val2)) {
+            return true;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 != $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) !== 0;
+        }
+        return $val1 !== $val2;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forMoreOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) || is_null($val2)) {
+            return false;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 > $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) > 0;
+        }
+        return strcmp($val1, $val2) > 0;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forMoreOrEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) && is_null($val2)) {
+            return true;
+        }
+        if(is_null($val1) || is_null($val2)) {
+            return false;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 >= $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) >= 0;
+        }
+        return strcmp($val1, $val2) >= 0;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forNotMoreOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) || is_null($val2)) {
+            return true;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 <= $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) <= 0;
+        }
+        return strcmp($val1, $val2) <= 0;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forLessOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) || is_null($val2)) {
+            return false;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 < $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) < 0;
+        }
+        return strcmp($val1, $val2) < 0;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forLessOrEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) && is_null($val2)) {
+            return true;
+        }
+        if(is_null($val1) || is_null($val2)) {
+            return false;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 <= $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) <= 0;
+        }
+        return strcmp($val1, $val2) <= 0;
+    }
+
+    /**
+     * @param $val1
+     * @param $val2
+     * @param bool $isAsNumbers
+     * @param bool $isCaseInsensitive
+     * @return bool
+     */
+    public static function forNotLessOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
+    {
+        if(is_null($val1) || is_null($val2)) {
+            return true;
+        }
+        if($isAsNumbers) {
+            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
+            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
+            return $val1 >= $val2;
+        }
+        $val2 = strval($val2);
+        $val1 = strval($val1);
+        if($isCaseInsensitive) {
+            return strcasecmp($val1, $val2) >= 0;
+        }
+        return strcmp($val1, $val2) >= 0;
+    }
+}
--- src/checker/politics/FieldWithFieldCondPolitic.php
+++ src/checker/politics/FieldWithFieldCondPolitic.php
@@ -4,6 +4,7 @@ namespace Mnemesong\OrmTest\checker\politics;
 
 use Mnemesong\Fit\conditions\abstracts\CondInterface;
 use Mnemesong\Fit\conditions\FieldWithFieldCond;
+use Mnemesong\OrmTest\checker\SimpleOperatorsMatch;
 use Mnemesong\OrmTest\checker\StructureMatchPoliticInterface;
 use Mnemesong\OrmTest\exceptions\UnknownOperatorException;
 use Mnemesong\Structure\Structure;
@@ -33,234 +34,23 @@ class FieldWithFieldCondPolitic implements StructureMatchPoliticInterface
         $operator = $cond->getOperator();
 
         if ($operator === '=') {
-            return $this->forEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '!=') {
-            return $this->forNotEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forNotEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '>') {
-            return $this->forMoreOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forMoreOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '>=') {
-            return $this->forMoreOrEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forMoreOrEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '!>') {
-            return $this->forNotMoreOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forNotMoreOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '<') {
-            return $this->forLessOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forLessOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '<=') {
-            return $this->forLessOrEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forLessOrEqOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         } elseif ($operator === '!<') {
-            return $this->forNotLessOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
+            return SimpleOperatorsMatch::forNotLessOperatorMatch($val1, $val2, $cond->isAsNumbers(), $cond->isCaseInsensitive());
         }
         throw UnknownOperatorException::operator($operator);
     }
 
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) && is_null($val2)) {
-            return true;
-        }
-        if(is_null($val1) || is_null($val2)) {
-            return false;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 == $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) === 0;
-        }
-        return $val1 === $val2;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forNotEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) && is_null($val2)) {
-            return false;
-        }
-        if(is_null($val1) || is_null($val2)) {
-            return true;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 != $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) !== 0;
-        }
-        return $val1 !== $val2;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forMoreOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) || is_null($val2)) {
-            return false;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 > $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) > 0;
-        }
-        return strcmp($val1, $val2) > 0;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forMoreOrEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) && is_null($val2)) {
-            return true;
-        }
-        if(is_null($val1) || is_null($val2)) {
-            return false;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 >= $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) >= 0;
-        }
-        return strcmp($val1, $val2) >= 0;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forNotMoreOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) || is_null($val2)) {
-            return true;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 <= $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) <= 0;
-        }
-        return strcmp($val1, $val2) <= 0;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forLessOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) || is_null($val2)) {
-            return false;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 < $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) < 0;
-        }
-        return strcmp($val1, $val2) < 0;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forLessOrEqOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) && is_null($val2)) {
-            return true;
-        }
-        if(is_null($val1) || is_null($val2)) {
-            return false;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 <= $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) <= 0;
-        }
-        return strcmp($val1, $val2) <= 0;
-    }
-
-    /**
-     * @param $val1
-     * @param $val2
-     * @param bool $isAsNumbers
-     * @param bool $isCaseInsensitive
-     * @return bool
-     */
-    protected function forNotLessOperatorMatch($val1, $val2, bool $isAsNumbers, bool $isCaseInsensitive): bool
-    {
-        if(is_null($val1) || is_null($val2)) {
-            return true;
-        }
-        if($isAsNumbers) {
-            Assert::numeric($val1, 'Try to comparing not numeric value "' . $val1 . '" as numeric');
-            Assert::numeric($val2, 'Try to comparing not numeric value "' . $val2 . '" as numeric');
-            return $val1 >= $val2;
-        }
-        $val2 = strval($val2);
-        $val1 = strval($val1);
-        if($isCaseInsensitive) {
-            return strcasecmp($val1, $val2) >= 0;
-        }
-        return strcmp($val1, $val2) >= 0;
-    }
 }
